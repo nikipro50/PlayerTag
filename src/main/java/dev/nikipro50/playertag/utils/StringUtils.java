02ae3b882497a2884aa1fd7d5202b317063ebf34
nikipro50 <castaldogianmaria5@gmail.com>
2025-02-16 02:06:33 +0100

    Release

--- src/main/java/dev/nikipro50/playertag/utils/StringUtils.java
+++ src/main/java/dev/nikipro50/playertag/utils/StringUtils.java
@@ -7,17 +7,17 @@ import java.util.regex.Pattern;
 
 public class StringUtils {
     public static String hex(String message) {
-        Pattern patter = Pattern.compile("#[a-fA-F0-9]{6}");
-        Matcher matcher = patter.matcher(message);
+        Pattern pattern = Pattern.compile("(#[a-fA-F0-9]{6})");
+        Matcher matcher = pattern.matcher(message);
         while (matcher.find()) {
             String hexCode = message.substring(matcher.start(), matcher.end());
             String replaceSharp = hexCode.replace('#', 'x');
             char[] ch = replaceSharp.toCharArray();
-            StringBuilder builder = new StringBuilder("");
+            StringBuilder builder = new StringBuilder();
             for (char c : ch) builder.append("&" + c);
             message = message.replace(hexCode, builder.toString());
-            matcher = patter.matcher(message);
+            matcher = pattern.matcher(message);
         }
-        return ChatColor.translateAlternateColorCodes('&', message).replace("&", "§");
+        return ChatColor.translateAlternateColorCodes('&', message).replace('&', '§');
     }
 }
